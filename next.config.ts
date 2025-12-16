23f74e7de764ba1650227b047a341a657be02f25
Alejandro Matutano <alejandro.matutano@evoluciona.com>
2025-12-17 00:23:14 +0100

    as

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from "next"
+
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    optimizePackageImports: ["lucide-react"],
+  },
+}
+
+export default nextConfig
